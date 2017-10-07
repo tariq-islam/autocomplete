02a13150b4eb5bdeedb258d31cb01978bd71b23c
tariq-islam <tariq.m.islam@gmail.com>
2017-10-07 13:33:42 -0400

    adding alternative parsing

--- workdir/jsontoarray.php
+++ workdir/jsontoarray.php
@@ -6,4 +6,8 @@ ini_set('memory_limit', '1024M');
 $shipments = json_decode(file_get_contents("products.json"), true);
 print_r($shipments);
 
+// alternatively...
+//
+// $shipments = json_encode(json_decode(file_get_contents("shipments.js"), true));
+// echo $shipments;
 ?>
